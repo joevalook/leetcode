851a1c4482a16c14836ab9070f898732731df4b8
Joe Valookaran <joevalookaran@hotmail.com>
2022-10-31 15:15:57 -0400

    added triple sum and started max area

---
+++ maxAreaWater.js
@@ -0,0 +1,13 @@
+var maxArea = function(height) {
+    let a = 0
+    for (let i =0; i < height.length - 1; i++) {
+      for (let j = i+1; j < height.length; j++) {
+        let h = Math.min(height[i], height[j])
+        let l = j-i
+        if (h * l > a) {
+          a = h*l
+        }
+      }
+    }
+    return a
+};
--- threeSum.js
+++ threeSum.js
@@ -1,36 +1,39 @@
-var threeSum = function(nums) {
-  let a = []
-  let zeros = 0
-    for (let i =0; i< nums.length-2; i++) {
-      for (let j = i+1; j<nums.length - 1; j++){
-        for (let k = j+1; k < nums.length; k++) {
-          if (nums[i] +nums[j] +nums[k] === 0) {
-            if (nums [i] === 0 && nums[j] === 0 && nums[k] === 0 && zeros === 0) {
-              a.push([nums[i], nums[j], nums[k]]) 
-              zeros = 1
-            }
-            else if (a.length === 0) {
-              a.push([nums[i], nums[j], nums[k]])
-            }
-
-            else {
-              let check = 0
-              for (let element of a) {
-                if (element.includes(nums[i]) && element.includes(nums[j]) && element.includes(nums[k])) {
-                  break;
-                }
-                else {
-                  check += 1
-                }
-              }
-              if (check === a.length) {
-                a.push([nums[i], nums[j], nums[k]])
-              }
-
-            }
-          }
+var threeSum = function (nums) {
+  let a = [];
+  let check = 0;
+  if (nums.length < 3) {
+    return ([]);
+  }
+  nums.sort((a, b) => a - b);
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (nums[i] > 0) {
+      return a;
+    }
+    let start = i + 1
+    let end = nums.length-1
+    while(start<end) {
+    if (nums[i] + nums[start] + nums[end] === 0) {
+      let answer = [nums[i],nums[start],nums[end]].sort();
+      for (let element of a) {
+        check = 0;
+        if (element[0] === answer[0] && element[1] === answer[1] && element[2] === answer[2]) {
+          check = 1;
+          break;
         }
       }
+      if (check === 0) {
+        a.push(answer);
+      }
+      start +=1
+      end -= 1
     }
-    return a
+    else if (nums[i] + nums[start] + nums[end] < 0) {
+      start +=1
+    }
+    else if (nums[i] + nums[start] + nums[end] > 0) {
+      end -=1;
+    }
+  }
+}
+  return a;
 };
