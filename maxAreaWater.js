27cd9ea28756d411aa8aef9aff752f9631f2b998
Joe Valookaran <joevalookaran@hotmail.com>
2022-10-31 15:31:22 -0400

    finished maxArea

--- maxAreaWater.js
+++ maxAreaWater.js
@@ -1,13 +1,15 @@
-var maxArea = function(height) {
-    let a = 0
-    for (let i =0; i < height.length - 1; i++) {
-      for (let j = i+1; j < height.length; j++) {
-        let h = Math.min(height[i], height[j])
-        let l = j-i
+var maxArea = function (height) {
+  let a = 0;
+  for (let i = 0; i < height.length - 1; i++) {
+    if (height[i] * (height.length - 1 - i) > a) {
+      for (let j = i + 1; j < height.length; j++) {
+        let h = Math.min(height[i], height[j]);
+        let l = j - i;
         if (h * l > a) {
-          a = h*l
+          a = h * l;
         }
       }
     }
-    return a
+  }
+  return a;
 };
